b1179375bc723351cb662eb3b99b184b8b5576d2
jak0b <me@jak0b.com>
2024-01-05 13:27:48 +0100

    chore(wezterm): add styles to iosevka font

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -21,6 +21,33 @@ elseif colorscheme == "light" then
   config.color_scheme = "Ayu Light (Gogh)"
 end
 
+local function iosevka_style(style)
+  -- soruce: https://github.com/be5invis/Iosevka/blob/main/doc/stylistic-sets.md
+   local styles = {
+    ["Default"]               = "ss00",
+    ["Andale Mono Style"]     = "ss01",
+    ["Anonymous Pro Style"]   = "ss02",
+    ["Consolas Style"]        = "ss03",
+    ["Menlo Style"]           = "ss04",
+    ["Fira Mono Style"]       = "ss05",
+    ["Liberation Mono Style"] = "ss06",
+    ["Monaco Style"]          = "ss07",
+    ["Pragmata Pro Style"]    = "ss08",
+    ["Source Code Pro Style"] = "ss09",
+    ["Envy Code R Style"]     = "ss10",
+    ["X Window Style"]        = "ss11",
+    ["Ubuntu Mono Style"]     = "ss12",
+    ["Lucida Style"]          = "ss13",
+    ["JetBrains Mono Style"]  = "ss14",
+    ["IBM Plex Mono Style"]   = "ss15",
+    ["PT Mono Style"]         = "ss16",
+    ["Recursive Mono Style"]  = "ss17",
+    ["Input Mono Style"]      = "ss18",
+    ["Curly Style"]           = "ss20",
+  }
+  return styles[style]
+end
+
 config.font_size = 20
 config.font = wezterm.font({
   family = "Iosevka Nerd Font",
