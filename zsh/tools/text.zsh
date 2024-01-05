559c412698384c34deb25c55e3fabd5b1f1d7802
jak0b <me@jak0b.com>
2024-01-05 13:30:10 +0100

    fix(tools/text): add `LC_ALL` env to fix locale settings in rand functions

--- zsh/tools/text.zsh
+++ zsh/tools/text.zsh
@@ -1,12 +1,12 @@
 function rand() {
   local length=${1:-64}
-  local pass=$(tr -cd '0-9a-zA-Z!#$%&*+-=?@^_|' < /dev/urandom | fold -w"$length" | head -n1)
+  local pass=$(LC_ALL=C tr -cd '0-9a-zA-Z!#$%&*+-=?@^_|' < /dev/urandom | fold -w"$length" | head -n1)
   echo $pass
 }
 
 function randa() {
   local length=${1:-64}
-  local pass=$(tr -cd '0-9a-zA-Z' < /dev/urandom | fold -w$length | head -n1)
+  local pass=$(LC_ALL=C tr -cd '0-9a-zA-Z' < /dev/urandom | fold -w$length | head -n1)
   echo $pass
 }
 
