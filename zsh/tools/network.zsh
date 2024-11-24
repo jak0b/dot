9e5e327e22c9c02340b17f921dde07502b774db5
jak0b <me@jak0b.com>
2024-11-24 12:12:50 +0000

    chore(zsh/network): rewrite zsh config
    remove wireguard file

--- zsh/tools/network.zsh
+++ zsh/tools/network.zsh
@@ -1,7 +1,30 @@
-pubip() {
-  local ip_version="${1:-64}"
-  local rsp=$(curl -s "https://api${ip_version}.ipify.org")
-  echo "$rsp"
+function pubip() {
+  local ip_version
+
+  ip_version="${1:-64}"
+  echo "$(curl -s "https://api${ip_version}.ipify.org")"
 }
 
+function ping-default-route() {
+  if >/dev/null declare -f -F default-route
+  then die 1 'default-route function muse be defined'
+  fi
+  ping "$@" "$(default-route)"
+}
 
+(( $+commands[wg] )) && {
+  function wg-pair() {
+    local priv_key pub_key
+    priv_key="$(wg genkey)"
+    pub_key="$(echo "$priv_key" | wg pubkey)"
+
+    if [[ $1 == '-f' ]]
+    then
+      echo "$priv_key" > key
+      echo "$pub_key" > pub
+    else
+      echo 'private key:' "$priv_key"
+      echo 'public key: ' "$pub_key"
+    fi
+  }
+}
