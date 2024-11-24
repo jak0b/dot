ca9a4166a2219881580a6f732f298919221e5da9
jak0b <me@jak0b.com>
2024-11-24 12:09:08 +0000

    chore(zsh/aws): rewrite aws shell config

--- zsh/tools/aws.zsh
+++ zsh/tools/aws.zsh
@@ -1,40 +1,48 @@
-if [ ! -d "${HOME}/.aws/.env" ]; then
-  mkdir -p "${HOME}/.aws/.env"
-fi
+function aws-load() {
+  if [ ! -d "${HOME}/.aws/.env" ]; then
+    mkdir -p "${HOME}/.aws/.env"
+  fi
 
-if [ -f "${HOME}/.aws/.env/AWS_PROFILE" ]; then
-  export AWS_PROFILE="$(< $HOME/.aws/.env/AWS_PROFILE)"
-fi
+  if [ -f "${HOME}/.aws/.env/AWS_PROFILE" ]; then
+    export AWS_PROFILE="$(< $HOME/.aws/.env/AWS_PROFILE)"
+  fi
 
-if [ -f "${HOME}/.aws/.env/AWS_REGION" ]; then
-  export AWS_REGION="$(< $HOME/.aws/.env/AWS_REGION)"
-fi
+  if [ -f "${HOME}/.aws/.env/AWS_REGION" ]; then
+    export AWS_REGION="$(< $HOME/.aws/.env/AWS_REGION)"
+  fi
+} && aws-load && unset -f aws-load
 
 function aws() {
   case $1 in
     profile)
-      if [ -n "$2" ] && [ "$2" = "current" ] && [ -f "$HOME/.aws/cli/current_profile"  ]; then
-        cat "$HOME/.aws/cli/current_profile"
+      if [[ $2 = "current" ]] && [[ -f $HOME/.aws/.env/AWS_PROFILE ]]
+      then cat "$HOME/.aws/.env/AWS_PROFILE"
       else
         local profiles=$(awk '/profile/{ print substr($2, 1, length($2)-1) }' ~/.aws/config)
-        local selected_profile=$(echo "$profiles" | fzf --header 'Select AWS profile' --preview 'cat ~/.aws/config') || return
-        echo "$selected_profile" > $HOME/.aws/cli/current_profile
-        export AWS_PROFILE="$selected_profile"
+        local selected=$(echo "$profiles" | fzf --header 'Select AWS profile') || die 'no profile selected'
+        echo "$selected" > $HOME/.aws/.env/AWS_PROFILE
+        export AWS_PROFILE="$selected"
       fi
       ;;
     region)
-      echo $AWS_REGION
+      if [[ $2 = "current" ]] && [[ -f $HOME/.aws/.env/AWS_REGION ]]
+      then cat "$HOME/.aws/.env/AWS_REGION"
+      else
+        local regions="$(curl -s 'https://raw.githubusercontent.com/boto/botocore/refs/heads/develop/botocore/data/endpoints.json' \
+                         | jq -r '.partitions[0].regions | to_entries | .[] | "\(.key)\t\(.value.description)"')"
+        local selected=$(echo "$regions" | fzf --header 'Select AWS region' | cut -f1) || die 'no profile selected'
+        echo "$selected" > $HOME/.aws/.env/AWS_REGION
+        export AWS_REGION="$selected"
+      fi
       ;;
-    *)
-      command aws "$@"
+    *) command aws "$@" ;;
   esac
 }
 
-s3-bucket-delete-all-versions() {
+function s3-bucket-delete-all-versions() {
   local bucket="$1"
-  if [ -z "$bucket" ]; then
-    echo "No bucket"
-    return 1
+  if [[ -z $bucket ]]
+  then die 'No bucket'
   fi
 
   local all_objects=$(aws s3api list-object-versions \
@@ -44,10 +52,20 @@ s3-bucket-delete-all-versions() {
   echo -n "$all_objects"
   # TODO:
   # fix string comparison in case the bucket is empty
-  if [ "$all_objects" = '{\n    "Objects": null\n}' ]; then
-    echo "Bucket has no versions"
-    return 2
+  if [[ $all_objects == '{\n    "Objects": null\n}' ]]
+  then die 2 'Bucket has no versions'
   fi
 
   aws s3api delete-objects --bucket "$bucket" --delete "$all_objects"
+}
+
+function s3-bucket-enable-versioning() {
+  local bucket="$1"
+  if [[ -z $bucket ]]
+  then die 'do bucket'
+  fi
+
+  aws s3api put-bucket-versioning \
+      --bucket "$bucket" \
+      --versioning-configuration Status=Enabled
 }
