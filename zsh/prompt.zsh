e37f8da02f4525e3d9aed0810a6c1c6149a5337b
jak0b <me@jak0b.com>
2025-02-21 13:23:47 +0100

    feat(zsh/propmt): simplyfy the prompt

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -1,17 +1,15 @@
 function setup_prompt() {
   autoload -U colors && colors
 
-  GIT_INFO_PREFIX='%F{yellow}⟪ '
-  GIT_INFO_SUFFIX=' ⟫ %f'
-
-  local arrow='➢'
-  local back_arrow='↲'
+  GIT_INFO_PREFIX='%F{yellow}{'
+  GIT_INFO_SUFFIX='}%f'
+  local arrow='>>'
 
   local prompt_user prompt_line prompt_host
   case "$UID" in
     0)
       prompt_user='%F{red}%n%f'
-      prompt_line="%F{red}${arrow} %f"
+      prompt_line="%F{red}${arrow}%f"
       ;;
     *)
       prompt_user='%F{green}%n%f'
@@ -26,12 +24,12 @@ function setup_prompt() {
     prompt_host='%F{green}%m%f'
   fi
 
-  local user_host="${prompt_user}%F{cyan}@${prompt_host}"
+  local user_host="[${prompt_user}%F{cyan}@${prompt_host}]"
   local dir_info="%B%F{blue}%~%f%b"
-  local return_code="%(?..%F{red}%? ${back_arrow} %f)"
+  local return_code="%(?..%F{red}%?%f)"
 
   local git_info='$(git_prompt_info)'
 
-  PROMPT="$(printf '%s %s %s\n%s ' "$user_host" "$dir_info" "$git_info" "$prompt_line")"
-  RPROMPT="$(printf '%s' "$return_code")"
+  # [j0b@njord] > /interface/
+  PROMPT="$(printf '%s %s %s%s\n%s' "$user_host" "$dir_info" "$git_info" "$return_code" "$prompt_line")"
 }
